30ae34c7f88535324b926769e13f4a9c9fcf7387
rajesh-rana01 <rajeshra5naa@gmail.com>
2026-01-13 19:46:18 +0545

    described interceptors

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -1,13 +1,18 @@
 import axios from 'axios'
 import { Server_URL } from './constants'
 
+// custom axios instance
 const axiosInstance = axios.create({
     baseURL: Server_URL,
-    timeout:10000,
+    timeout:10000, //10 sec
     headers:{
         "Content-Type":"application/json"
     }
 })
+
+//interceptor checks if a token exists in localStorage.
+//if found then it adds Authorization: Bearer <token> to the request headers.
+//This way, every request automatically includes the token for authentication
 
 axiosInstance.interceptors.request.use(
     (config)=>{
