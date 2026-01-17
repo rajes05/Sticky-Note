a40f4cac34e4913fea18e01b78ff8f8e740cc97b
rajesh-rana01 <rajeshra5naa@gmail.com>
2026-01-17 23:12:36 +0545

    profile

--- frontend/src/components/Cards/Profile.jsx
+++ frontend/src/components/Cards/Profile.jsx
@@ -1,5 +1,26 @@
-export default function Profile(){
+import { getInitials } from "../../utils/helper"
+
+export default function Profile({userInfo, onLogout}){
     return(
-        <div>Profile</div>
+        <div className="flex items-center gap-3 bg-white rounded-xl p-3 shadow-sm border border-[#DCFCE7]">
+
+            <div className="w-12 h-12 flex items-center justify-center rounded-full text-[#16A34A] font-semibold bg-[#DCFCE7]">
+
+                {getInitials(userInfo?.fullName)}
+
+            </div>
+
+            <div>
+                <p className="text-sm font-medium text-[#1E293B]">
+                    {userInfo.fullName}
+                </p>
+                <button className="text-sm text-[#B45309] hover:underline hover:text-[#16A34A] transition-colors duration-200 cursor-pointer"
+                onClick={onLogout}
+                >
+                    Logout
+                </button>
+            </div>
+
+        </div>
     )
 }
