caa196484b73b66e726019a946079d3c2b51a4ba
rajesh-rana01 <rajeshra5naa@gmail.com>
2026-01-18 00:17:48 +0545

    empty card

--- frontend/src/components/EmptyCard/EmptyCard.jsx
+++ frontend/src/components/EmptyCard/EmptyCard.jsx
@@ -1,5 +1,29 @@
-export default function EmptyCard(){
+
+
+export default function EmptyCard({ imgSrc = '/notes.jpg', message }){
+    
     return(
-        <div>EmptyCard</div>
+        <div className="flex flex-col items-center justify-center mt-20 text-center">
+
+            <img 
+                src={imgSrc}
+                alt="No notes"
+                className="w-64 sm:w-72 opacity-95 drop-shadow-md rounded-xl"
+                />
+
+                <p className="w-4/5 sm:w-1/2 text-sm sm:text-base font-medium leading-7 mt-6 text-[#1E293B]">
+                    <span className="block text-[#16A34A] font-semibold text-lg mb-1">
+                        Nothing here yet *
+                    </span>
+                    <span className="text-[#B45309]">
+                        {message ||
+                        "Start by adding your first note - your ideas deserve a place to grow!"}
+                    </span>
+                </p>
+                <p className="mt-3 text-xs text-[#6474BB] italic">
+                    Click the <span className="text-[#16A34A] font-semibold">'+'</span>{' '}
+                    button below to create a new note.
+                </p>
+        </div>
     )
 }
